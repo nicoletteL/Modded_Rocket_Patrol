cc385f78756b82e15fb60648cd29c659f7a8f48e
nicoletteL <nantisde@ucsc.edu>
2022-04-14 21:06:29 -0700

    Finished changing assets

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,7 +7,7 @@ class Play extends Phaser.Scene {
     // load images/tile sprites
     this.load.image('rocket', './assets/rocket.png');
     this.load.image('spaceship', './assets/spaceship.png');
-    this.load.image('starfield', './assets/starfield.png');
+    this.load.image('starfield', './assets/starfield2.png');
 
     // load spritesheet
     this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32,
@@ -31,9 +31,9 @@ class Play extends Phaser.Scene {
     this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
     // add spaceships (x3)
-    this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0,0);
-    this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
-    this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+    this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*3, 'spaceship', 0, 30).setOrigin(0,0);
+    this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*4 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+    this.ship03 = new Spaceship(this, game.config.width, borderUISize*5 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
 
   //   // define keys
    keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -55,16 +55,11 @@ class Play extends Phaser.Scene {
     let scoreConfig = {
       fontFamily: 'Courier',
       fontSize: '28px',
-      backgroundColor: '#F3B141',
-      color: '#843605',
-      align: 'right',
-      padding: {
-        top: 5,
-        bottom: 5,
-      },
-      fixedWidth: 100
+      color: '#FFFFFF',
+      align: 'left',
+      fixedWidth: 200
     }
-    this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+    this.scoreLeft = this.add.text(borderUISize, borderUISize, "Score: ".concat(this.p1Score), scoreConfig);
   
     // GAME OVER flag
     this.gameOver = false;
@@ -142,6 +137,6 @@ class Play extends Phaser.Scene {
 
     // score add and repaint
     this.p1Score += ship.points;
-    this.scoreLeft.text = this.p1Score; 
+    this.scoreLeft.text = "Score: ".concat(this.p1Score); 
   }
 }
